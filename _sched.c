f8f010efd261947af1a3b42cfd7addc6b3656ab8
shownlin <shownlin001cm@gmail.com>
2019-04-30 21:54:24 +0800

    Update _sched.c

--- _sched.c
+++ _sched.c
@@ -1,16 +1,4 @@
 #define _GNU_SOURCE
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
-#include <sched.h>
-#include <sys/time.h>
-#include <linux/kernel.h>
-#include <sys/syscall.h>
-#include <semaphore.h>
-#include <math.h>
 #include "list.h" 
 #include "_sched.h"
 
@@ -18,11 +6,96 @@
 // 1->FIFO, 2->RR
 int POLICY[2] = {1, 2};
 
+struct ready_queue * check_preempt(struct ready_queue *ready, struct ready_queue *tmp, unsigned long local_clock, int * preempt){
+	struct ready_queue *tmp1;
+	
+	tmp1 = list_entry(ready->list.next, struct ready_queue, list);
+
+	if(tmp != tmp1){
+		if(tmp1->start + tmp1->exe - local_clock > tmp->exe){
+			tmp1->exe = tmp1->start + tmp1->exe - local_clock;
+			tmp1->start = -1;
+			*preempt = 1;
+			list_del(&(tmp->list));
+			list_add(&(tmp->list), &(ready->list));
+			tmp->start = local_clock;
+		}
+	}
+	else{
+		*preempt = 1;
+	}
+	return tmp1;
+}
+
+
+struct ready_queue * find_shortest(struct ready_queue *ready){
+	long long shortest = 100000000000; 
+	struct list_head *shortest_pos, *pos;
+	struct ready_queue *tmp;
+	list_for_each(pos, &(ready->list)){
+		tmp = list_entry(pos, struct ready_queue, list);
+		if( tmp->exe < shortest ){
+			shortest = tmp->exe;
+			shortest_pos = pos;
+		}
+	}
+	tmp = list_entry(shortest_pos, struct ready_queue, list);
+	list_del(&(tmp->list));
+	list_add(&(tmp->list), &(ready->list));
+	
+	return tmp;
+}
+
 // 1 -> ascending, 0 -> descending
 void swap(int * a, int * b){
 	int c = *a;
 	*a = *b;
 	*b = c;
+}
+
+void inverse_permutation(const int a[], int b[], int N){
+	int count = 0, cursor = 0, probe = 0;
+	for(unsigned i = 0; i < N; ++i)
+		b[i] = -1;
+	
+	while(count < N){
+		if(b[a[probe]] == -1){
+			b[a[probe]] = probe;
+			probe = a[probe];
+			++count;
+		}
+		else{
+			for(; cursor < N; ++cursor)
+				if(b[cursor] == -1){
+					probe = cursor;
+					break;
+				}
+		}
+	}
+}
+
+
+void check_terminate(struct ready_queue *ready, struct sched_param *param, unsigned long local_clock, int *flag){
+	struct ready_queue *tmp;
+
+	tmp = list_entry(ready->list.next, struct ready_queue, list);
+
+	if(tmp->start + tmp->exe == local_clock){
+		list_del(&(tmp->list));
+		if(!list_empty(&ready->list)){
+			tmp = find_shortest(ready);
+			tmp->start = local_clock;
+			if(tmp->pid){
+		 		if(sched_setscheduler(tmp->pid, SCHED_FIFO, param)){
+					printf("3 sched_setscheduler error: %s\n", strerror(errno));
+					exit(1);
+				}
+			}
+			else{
+				*flag = 1;
+			}
+		}
+	}
 }
 
 void sort(int value[], int index[], int N, int ascend){
@@ -44,27 +117,6 @@ void sort(int value[], int index[], int N, int ascend){
 					swap(&index[i], &index[j]);
 				}
 			}
-		}
-	}
-}
-
-void inverse_permutation(const int a[], int b[], int N){
-	int count = 0, cursor = 0, probe = 0;
-	for(unsigned i = 0; i < N; ++i)
-		b[i] = -1;
-	
-	while(count < N){
-		if(b[a[probe]] == -1){
-			b[a[probe]] = probe;
-			probe = a[probe];
-			++count;
-		}
-		else{
-			for(; cursor < N; ++cursor)
-				if(b[cursor] == -1){
-					probe = cursor;
-					break;
-				}
 		}
 	}
 }
@@ -108,72 +160,11 @@ void resort(int value[], int index[], int N, int ascend, const int T[], const in
         }
 }
 
-struct ready_queue * find_shortest(struct ready_queue *ready){
-	long long shortest = 100000000000; 
-	struct list_head *shortest_pos, *pos;
-	struct ready_queue *tmp;
-	list_for_each(pos, &(ready->list)){
-		tmp = list_entry(pos, struct ready_queue, list);
-		if( tmp->exe < shortest ){
-			shortest = tmp->exe;
-			shortest_pos = pos;
-		}
-	}
-	tmp = list_entry(shortest_pos, struct ready_queue, list);
-	list_del(&(tmp->list));
-	list_add(&(tmp->list), &(ready->list));
-	
-	return tmp;
-}
-
-void check_terminate(struct ready_queue *ready, struct sched_param *param, unsigned long local_clock, int *flag){
-	struct ready_queue *tmp;
-
-	tmp = list_entry(ready->list.next, struct ready_queue, list);
-
-	if(tmp->start + tmp->exe == local_clock){
-		list_del(&(tmp->list));
-		if(!list_empty(&ready->list)){
-			tmp = find_shortest(ready);
-			tmp->start = local_clock;
-			if(tmp->pid){
-		 		if(sched_setscheduler(tmp->pid, SCHED_FIFO, param)){
-					printf("3 sched_setscheduler error: %s\n", strerror(errno));
-					exit(1);
-				}
-			}
-			else{
-				*flag = 1;
-			}
-		}
-	}
-}
-struct ready_queue * check_preempt(struct ready_queue *ready, struct ready_queue *tmp, unsigned long local_clock, int * preempt){
-	struct ready_queue *tmp1;
-	
-	tmp1 = list_entry(ready->list.next, struct ready_queue, list);
-
-	if(tmp != tmp1){
-		if(tmp1->start + tmp1->exe - local_clock > tmp->exe){
-			tmp1->exe = tmp1->start + tmp1->exe - local_clock;
-			tmp1->start = -1;
-			*preempt = 1;
-			list_del(&(tmp->list));
-			list_add(&(tmp->list), &(ready->list));
-			tmp->start = local_clock;
-		}
-	}
-	else{
-		*preempt = 1;
-	}
-	return tmp1;
-}
-
 void check_remain(struct ready_queue *ready, struct sched_param *param, unsigned long * local_clock){
 	struct ready_queue *tmp;
 	tmp = list_entry(ready->list.next, struct ready_queue, list);
 	while(*local_clock < tmp->start + tmp->exe){
-		wait_one_unit;
+		wait_unit_1;
 		++(*local_clock);
 	}
 	
@@ -254,14 +245,14 @@ int main(){
 
 	for(; i < N; ++i){
 		int empty, preempt = 0;
-		int this_pid, flag = 0;
+		int mypid, flag = 0;
 		
 		// wait to be forked
 		while(local_clock != R[i]){
 			if((S[0] == 'S' || S[0] == 'P') && !list_empty(&ready.list))
 				check_terminate(&ready, &param, local_clock, &flag);
 			
-			wait_one_unit;
+			wait_unit_1;
 			++local_clock;
 		}
 			
@@ -280,18 +271,18 @@ int main(){
 			}
 		}
 
-		syscall(336, 1, this_pid, &start);
+		syscall(336, 1, mypid, &start);
 
 		pid = fork();
 		if(!pid){
-			this_pid = getpid();
+			mypid = getpid();
 			printf("%s %d\n", P[R_index[i]], getpid());
 			
 			for(unsigned long _i = 0; _i < T[T_inverse[R_index[i]]]; ++_i){
-				wait_one_unit;
+				wait_unit_1;
 			}
 	
-			syscall(336, 0, this_pid, &start);
+			syscall(336, 0, mypid, &start);
 			exit(0);
 		}
 		else if(pid == -1){
